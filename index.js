f0770a16b417dc1ffac5f6fd2c66b8db60bb971e
Lilibeth Mejia <mejialily036@gmail.com>
2024-02-09 14:44:25 -0600

    added audio files to javascript

--- index.js
+++ index.js
@@ -4,15 +4,20 @@ console.log(gamePattern);
 
 
 function nextSequence() {
-    const randomNum= Math.ceil(Math.random() * 3);
+    const randomNum= Math.ceil(Math.random() * 4);
     console.log(randomNum);
 
-    for (let i= 0; i < buttonColors.length; i++) {
-        let randomChosenColor= buttonColors[randomNum];
-        console.log(randomChosenColor);
-        gamePattern.push(randomChosenColor);
-    }
+    const randomChosenColor= buttonColors[randomNum];
+    console.log(randomChosenColor);
 
+    gamePattern.push(randomChosenColor);
+
+    $("#" + randomChosenColor).fadeIn(100).fadeOut(100).fadeIn(100);
+    
+    const audio = new Audio("./sounds/" + randomChosenColor + ".mp3");
+    audio.play();
+
+    
 
 };
 
